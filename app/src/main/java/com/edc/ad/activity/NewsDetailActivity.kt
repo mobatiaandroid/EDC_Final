3b79206abb94f2237f0bb806a1685243516200fe
SanjuSabu2 <sabusanju2@gmail.com>
2022-01-07 12:09:27 +0530

    News Detail Layout Added

--- app/src/main/java/com/edc/ad/activity/NewsDetailActivity.kt
+++ app/src/main/java/com/edc/ad/activity/NewsDetailActivity.kt
@@ -1,12 +1,40 @@
 package com.edc.ad.activity
 
+import android.content.ClipDescription
+import android.content.Context
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import com.edc.ad.R
 
 class NewsDetailActivity : AppCompatActivity() {
+    lateinit var context: Context
+    lateinit var image: String
+    lateinit var description: String
+    lateinit var title: String
+    lateinit var textViewTitle: TextView
+    lateinit var backButton: ImageView
+    lateinit var shareButton: ImageView
+    var position = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_detail)
+        context = this
+        initialiseUI()
+    }
+
+    private fun initialiseUI() {
+
+        val incomingIntent = intent
+        position = incomingIntent.extras!!.getInt("position")
+        image = incomingIntent.extras!!.getString("image").toString()
+        description = incomingIntent.extras!!.getString("description").toString()
+        title = incomingIntent.extras!!.getString("title").toString()
+        textViewTitle = findViewById(R.id.title)
+        backButton = findViewById(R.id.backBtn)
+        shareButton = findViewById(R.id.shareBtn)
+        textViewTitle.text = title
     }
 }
