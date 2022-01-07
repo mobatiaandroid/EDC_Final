955245c71175e38b160a7b48c163c8b8e10a50c6
SanjuSabu2 <sabusanju2@gmail.com>
2022-01-07 13:05:48 +0530

    Completed News Detail Activity

--- app/src/main/java/com/edc/ad/activity/NewsDetailActivity.kt
+++ app/src/main/java/com/edc/ad/activity/NewsDetailActivity.kt
@@ -1,22 +1,26 @@
 package com.edc.ad.activity
 
-import android.content.ClipDescription
 import android.content.Context
-import android.media.Image
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.edc.ad.R
+import kotlinx.android.synthetic.main.activity_news_detail.*
 
 class NewsDetailActivity : AppCompatActivity() {
     lateinit var context: Context
-    lateinit var image: String
+    lateinit var imageURL: String
     lateinit var description: String
     lateinit var title: String
     lateinit var textViewTitle: TextView
     lateinit var backButton: ImageView
-    lateinit var shareButton: ImageView
+//    lateinit var shareButton: ImageView
+    lateinit var textViewDescription: TextView
+    lateinit var imageView: ImageView
     var position = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +33,25 @@ class NewsDetailActivity : AppCompatActivity() {
 
         val incomingIntent = intent
         position = incomingIntent.extras!!.getInt("position")
-        image = incomingIntent.extras!!.getString("image").toString()
+        imageURL = incomingIntent.extras!!.getString("image").toString()
         description = incomingIntent.extras!!.getString("description").toString()
         title = incomingIntent.extras!!.getString("title").toString()
         textViewTitle = findViewById(R.id.title)
         backButton = findViewById(R.id.backBtn)
-        shareButton = findViewById(R.id.shareBtn)
+//        shareButton = findViewById(R.id.shareBtn)
+        imageView = findViewById(R.id.image)
+        textViewDescription = findViewById(R.id.description)
         textViewTitle.text = title
+        textViewDescription.text = description
+        Glide
+            .with(context)
+            .load(imageURL)
+            .centerCrop()
+            .into(imageView);
+        backButton.setOnClickListener { finish() }
+//        shareButton.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_SEND)
+//            startActivity(Intent.createChooser(intent, "Share Using"))
+//        }
     }
 }
