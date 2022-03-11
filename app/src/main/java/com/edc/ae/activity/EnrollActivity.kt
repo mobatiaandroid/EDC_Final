979544af66c9879ff62108678c6558b323235175
SanjuSabu2 <sabusanju2@gmail.com>
2022-03-11 18:27:58 +0530

    Enroll Changes

--- app/src/main/java/com/edc/ae/activity/EnrollActivity.kt
+++ app/src/main/java/com/edc/ae/activity/EnrollActivity.kt
@@ -415,6 +415,7 @@ class EnrollActivity : AppCompatActivity() {
                             startActivity(intent)
                         } else {
                             CommonMethods.showLoginErrorPopUp(context,"",call.data.message.toString())
+                            finish()
                         }
 
 
